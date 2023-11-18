b52aed99eb7bc5920cf9b298c1c9c9e79f35f1a6
Kevin Martinez <82669898+kadm91@users.noreply.github.com>
2023-11-18 12:51:22 -0700

    Initial Commit

---
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by Kevin Martinez on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
