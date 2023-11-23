42f237a20233f26818791adaeaea19dd2b7b9867
Kevin Martinez <82669898+kadm91@users.noreply.github.com>
2023-11-22 20:22:49 -0700

    Integrate sign in wiht google

---
+++ FirebaseBootcamp/Authentication/SignInGoogleHelper.swift
@@ -0,0 +1,8 @@
+//
+//  SignINGoogleHelper.swift
+//  FirebaseBootcamp
+//
+//  Created by Kevin Martinez on 11/22/23.
+//
+
+import Foundation
---
+++ FirebaseBootcamp/Utilities/Utilities.swift
@@ -0,0 +1,8 @@
+//
+//  Utilities.swift
+//  FirebaseBootcamp
+//
+//  Created by Kevin Martinez on 11/22/23.
+//
+
+import Foundation
